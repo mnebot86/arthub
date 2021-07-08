5280c45bca9c8896ba22e41b0709ea1e5dd47b46
mnebot <miguelnbt@icloud.com>
2021-07-07 22:43:53 -0400

    update home css

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,29 +1,23 @@
-import './Home.css'
+import "./Home.css";
 import React from "react";
 
 const Home = () => {
   return (
     <article>
       <div className="article-wrap">
-
-      <h1 className="article-title">
-        Welcome To The Hub!
-      </h1>
-      <br />
-      <p className="article-context">
-        Arthub is place for the creatives, the wonderers , imaginative, and the
-        dreamers. Here you can share your artworks, photographs, and film
-        creations. Everyone see the world differently and has their own
-        interpretation of it. Let others see your visions, but take the time to
-        look at other works of art as well. Lets do our best to make this a
-        strong and welcoming community. Feel free to post, like and comment on
-        each others art work!
-      </p>
+        <h1 className="article-title">Welcome To The Hub!</h1>
+        <p className="article-context">
+          Arthub is place for the creatives, the wonderers, imaginative, and
+          the dreamers. Here you can share your artworks, photographs, and film
+          creations. Everyone see the world differently and has their own
+          interpretation of it. <br/>Let others see your visions, but take the time
+          to look at other works of art as well. Lets do our best to make this a
+          strong and welcoming community. Feel free to post, like and comment on
+          each others art work!
+        </p>
       </div>
-    
     </article>
   );
 };
-
 
 export default Home;
